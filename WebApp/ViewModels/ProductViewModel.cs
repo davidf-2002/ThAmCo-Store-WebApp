35ca89f864775d2bae70f1692d1e3c727a830758
davidf-2002 <140307515+davidf-2002@users.noreply.github.com>
2024-12-22 03:38:51 +0000

    Untrack files now ignored by .gitignore

--- WebApp/ViewModels/ProductViewModel.cs
+++
@@ -1,7 +0,0 @@
-public class ProductViewModel
-{
-    public int Id { get; set; }
-    public string? Name { get; set; }
-    public string? Description { get; set; }
-    public decimal Price { get; set; }
-}
